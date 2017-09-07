a7b64e2e7c124b883ae6a394857cff54af3c2552
Frank Magazu <fmgames314@gmail.com>
2017-09-07 04:29:29 -0400

    Automated Commit

---
+++ Launcher/updater.py
@@ -0,0 +1,20 @@
+import urllib
+import subprocess  
+import os
+version = "";
+if(os.path.isfile("version.txt")):
+	version = open("version.txt").read()
+link = "https://github.com/fmgames314/U_HomeCommander/raw/master/README.md"
+f = urllib.urlopen(link)
+myfile = f.read()
+if(os.path.isfile("version.txt")):
+	os.remove("version.txt")
+file = open('version.txt', 'w')
+file.write(myfile)
+file.close()
+print (version)
+print (myfile)
+print (version != myfile)
+if(version != myfile):
+	urllib.urlretrieve ("https://github.com/fmgames314/U_HomeCommander/raw/master/U_HomeCommander.exe", "U_Home_Commander.exe")
+subprocess.Popen([r"U_Home_Commander.exe"])  
