f74c3b709b4c5cba98ea948d8cec3a65291cd19f
Frank Magazu <fmgames314@gmail.com>
2017-09-07 04:11:23 -0400

    Automated Commit

--- uploadU_HomeCommander.sh
+++ uploadU_HomeCommander.sh
@@ -1,6 +1,6 @@
 now="$(date)"
 your_date=now
-sed -i "s/^Current date.*/Current date ${your_date}/" /README.md
+sed -i "s/^Current date.*/Current date ${your_date}/" README.md
 git add .
 git commit -m "Automated Commit"
 git push origin master
